4f698eec96fa2dbb2d158e592f219bc62ed6c031
DeadKennedyx <juan.dw.ft@gmail.com>
2019-01-16 17:12:11 -0600

    chore/add-rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   has_many :user_tasks
   has_many :tasks, through: :user_tasks
   has_many :projects, through: :tasks
+
+  validates :name, uniqueness: true
+  validates :email, uniqueness: true
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+
+  factory :user do
+    email { "joe@gmail.com" }
+    password { "test123" }
+  end
+
+end
